d1755dbfe7621f8b610f00b1b334caa3c7392247
Silvio Sabo <silvio.sabo@waya.se>
2024-12-02 21:51:14 +0100

    add example for authenticated handler

---
+++ examples/authenticated/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/sillen102/simba"
+)
+
+type RequestBody struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type ResponseBody struct {
+	Message string `json:"message"`
+}
+
+// User is a struct that represents a user
+// in this example we use a simple struct to represent a user
+type User struct {
+	ID   int
+	Name string
+	Role string
+}
+
+// authFunc is a function that authenticates and returns a user
+// in this example we just return a hard-coded user
+func authFunc(r *http.Request) (*User, error) {
+	return &User{
+		ID:   1,
+		Name: "John Doe",
+		Role: "admin",
+	}, nil
+}
+
+func authenticatedHandler(ctx context.Context, req *simba.Request[simba.NoBody, simba.NoParams], user *User) (*simba.Response, error) {
+	return &simba.Response{
+		Body: ResponseBody{
+			Message: fmt.Sprintf("Hello %s, you are an %s", user.Name, user.Role),
+		},
+		Status: http.StatusOK, // We can omit this and it will default to 200 OK if the body is not nil and there is no error
+	}, nil
+}
+
+func main() {
+	// the router will use the authFunc to authenticate and retrieve the user
+	// for each request that uses the AuthenticatedHandlerFunc and pass it to the handler
+	router := simba.NewRouterWithAuth[User](authFunc)
+	router.GET("/user", simba.AuthenticatedHandlerFunc(authenticatedHandler))
+	http.ListenAndServe(":9999", router)
+}
