f0f0151bfc67e1e6724a9b51c3637f86c6e4fbfd
Silvio Sabo <silvio.sabo@gmail.com>
2024-12-16 18:56:08 +0100

    fix bug with settings not being injected to context

--- request.go
+++ request.go
@@ -33,7 +33,7 @@ func closeRequestBody(next http.Handler) http.Handler {
 }
 
 // injectRequestSettings injects the application Config into the Request context
-func injectRequestSettings(next http.Handler, settings settings.Request) http.Handler {
+func injectRequestSettings(next http.Handler, settings *settings.Request) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.WithValue(r.Context(), simbaContext.RequestSettingsKey, settings)
 		next.ServeHTTP(w, r.WithContext(ctx))
